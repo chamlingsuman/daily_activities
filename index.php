b768df1cff12fa5fcc1d0d2cb57a76d1ec0a21bb
chamlingsuman <mack30324@gmail.com>
2023-12-29 14:43:59 +0545

    create login form

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <body style="background: #f5f2ea">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+            <title>Daily Activities</title>
+       </head>
+
+       <div class="container d-flex flex-column">
+        <div class="row justify-content-center pt-5 px-5">
+            <div class="col-md-4">
+                <form method="post" action="login.php" class="border rounded m-3 p-5 bg-white">
+                    <h1 class="text-center text-primary font-weight-bold">Log in</h1>
+                    <p class="text-center">Enter your details to get signed<br>in to your account</p>
+
+
+                    <div class="mb-3">
+                       <strong><label for="username" class="form-label">Username</label></strong>
+                       <input type="text" class="form-control" name="username" placeholder="Enter your Username">
+                   </div>
+                   <div class="mb-3">
+                        <strong><label for="password" class="form-label">Password</label></strong>
+                        <input type="text" class="form-control" name="password" placeholder="Enter your Password">
+                   </div>
+                   <div class="row mx-0 mb-3">
+                    <button type="submit" class="btn btn-primary">Log in</button>
+                   </div>
+
+                   <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?></p>
+                    <?php } ?>
+
+                   <div class="row">
+                      <p class="text-center">Don't have an account? <strong><a href="register.php" class="text-primary text-decoration-none">Register</a></strong></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+       </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,96 @@
+<?php 
+$sname = "localhost";
+$u_name = "root";
+$password = "";
+$db_name = "daily_activities";
+
+$conn = mysqli_connect($sname, $u_name, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+}
+
+?>
+
+<?php 
+
+session_start(); 
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['username']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['user_name'] = $row['user_name'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: home.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+   }else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
